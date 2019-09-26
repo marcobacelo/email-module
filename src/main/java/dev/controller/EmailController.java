0463a2a2e068d6435f6341e210aabb2d223b8931
Marco Bacelo <marcobacelo90@gmail.com>
2019-09-26 02:05:35 -0300

    Update Controller

--- src/main/java/dev/controller/EmailController.java
+++ src/main/java/dev/controller/EmailController.java
@@ -3,23 +3,28 @@ package dev.controller;
 import dev.exceptions.InternalErrorException;
 import dev.model.Email;
 import dev.services.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/send_email", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/mail", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class EmailController {
 
-    @Autowired
     EmailService emailService;
 
     @PostMapping("/send_mail")
     public ResponseEntity<?> sendMail(@RequestBody Email email)
             throws InternalErrorException {
 
-        return ResponseEntity.ok("REQUISIÇÃO FUNFOU !! BOA DEV");
+        String subject = email.getSubject();
+        String address = email.getAddress();
+        String text = email.getText();
+
+        return ResponseEntity.ok("Requisição feita !!"
+            + "\nSubject: " + subject
+            + "\nEndereço: " + address
+            + "\nTexto: " + text);
     }
 }
